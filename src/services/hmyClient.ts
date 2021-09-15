f400980995df99e3cb9baabb886dd4c9272cf3b6
Sergey Karasev <ahiipsa@gmail.com>
2021-09-15 22:21:44 +0300

    added hmy client

--- src/services/hmyClient.ts
+++ src/services/hmyClient.ts
@@ -3,16 +3,17 @@ import * as onebtcSdk from 'onebtc.sdk';
 let client: onebtcSdk.IHmyClient = null;
 
 export async function getHmyClient() {
-
   if (client) {
     return client;
   }
 
+  // new 0xBffa908aC951eD4fa224bd28f1291280E4220825
+  // old 0x45b24bE9F317054B4D5972E9d685f6e403772f6b
   const hmyClient = await onebtcSdk.getHmyClient({
-    sdk: "web3",
-    nodeURL: "https://api.s0.b.hmny.io",
+    sdk: 'web3',
+    nodeURL: 'https://api.s0.b.hmny.io',
     chainId: 2,
-    contractAddress: "0x45b24bE9F317054B4D5972E9d685f6e403772f6b",
+    contractAddress: '0xBffa908aC951eD4fa224bd28f1291280E4220825',
     gasLimit: 6721900,
   });
 
