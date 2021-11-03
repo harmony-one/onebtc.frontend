6b16056651318773ee2a3f4d4158fae4fbda2297
ahiipsa <ahiipsa@gmail.com>
2021-11-03 22:38:30 +0300

    Fixed vault identity label

--- src/pages/Redeem/components/RedeemForm/RedeemForm.tsx
+++ src/pages/Redeem/components/RedeemForm/RedeemForm.tsx
@@ -106,7 +106,7 @@ export const RedeemForm: React.FC<Props> = observer(() => {
       />
 
       <Select
-        label="Vault identity"
+        label="Vault"
         name="vaultId"
         style={{ width: '100%' }}
         rules={[isRequired]}
