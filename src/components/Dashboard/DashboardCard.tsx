05eb5c41061c747d8f6e39641eea94090854dc17
ahiipsa <ahiipsa@gmail.com>
2021-11-25 01:38:55 +0300

    Fixed DashboardVaultsPage.tsx, used new cards layout

--- src/components/Dashboard/DashboardCard.tsx
+++ src/components/Dashboard/DashboardCard.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BoxTypes, Card } from 'grommet';
 
-type Props = {} & Pick<BoxTypes, 'pad' | 'fill' | 'width'>;
+type Props = {} & Pick<BoxTypes, 'pad' | 'fill' | 'width' | 'height'>;
 
 export const DashboardCard: React.FC<Props> = ({
   children,
   pad = 'none',
   width = 'medium',
+  height = 'medium',
 }) => {
   return (
-    <Card background="white" width={width} height="medium" pad={pad}>
+    <Card background="white" width={width} height={height} pad={pad}>
       {children}
     </Card>
   );
