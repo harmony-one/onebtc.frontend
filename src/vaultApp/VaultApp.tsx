8eedc333a0a4b2defb85be35db31a1dd4ebcf804
ahiipsa <ahiipsa@gmail.com>
2022-02-07 19:40:42 +0300

    Removed WatcherDashboard for VaultApp

--- src/vaultApp/VaultApp.tsx
+++ src/vaultApp/VaultApp.tsx
@@ -9,7 +9,6 @@ import { routes } from './routes/routes';
 import { InitializationPage } from './pages/initialization/InitializationPage';
 import { VaultDetailsPage } from './pages/vaultDetails/VaultDetailsPage';
 import { WatcherBalance } from '../components/WatcherBalance';
-import { WatcherDashboard } from '../modules/dashboard/WatcherDashboard';
 import { WatcherBTCNode } from '../modules/btcNode/WatcherBTCNode';
 import { ActionModals } from '../components/ActionModals';
 import { RegistrationPage } from './pages/registration/RegistrationPage';
@@ -35,7 +34,6 @@ const VaultApp: React.FC = () => (
         <Redirect to={routes.init} />
       </Switch>
       <WatcherBalance />
-      <WatcherDashboard />
       <WatcherBTCNode />
       <WatcherVaultInfo />
       <ActionModals />
