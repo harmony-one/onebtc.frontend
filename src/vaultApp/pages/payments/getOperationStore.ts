612045bf9fdbc0a4da7933e70d37ebddc2effa58
yuriy <yuriy.menkov@latoken.com>
2022-03-28 17:55:18 +0300

    fix

--- src/vaultApp/pages/payments/getOperationStore.ts
+++ src/vaultApp/pages/payments/getOperationStore.ts
@@ -4,17 +4,22 @@ import { Operation, vaultClient } from '../../modules/vaultClient/VaultClient';
 
 let cache: ListStoreConstructor<Operation> | null = null;
 
+let vaultAddress  = '';
+
 export const getOperationListStore = () => {
   if (cache) {
     return cache;
   }
 
-  const loadFn = params => {
-    return vaultClient.loadPayments({
+  const loadFn = async params => {
+    if (!vaultAddress) {
+      const info = await vaultClient.loadInfo();
+      vaultAddress = info.vaultAddress;
+    }
+
+    return await vaultClient.loadPayments({
       ...params,
-      // vault: '0x09987640F35EAdA8801D13455A468E02ff964Dcd',
-      vault: '0x297816FAEDB34eb8720dc6F5f001Ed64A5a83a9d',
-      // vault: '0x22b7349c260277337b7E773dd223Cf04b25A6eE5'
+      vault: vaultAddress,
     });
   };
 
